8a981dcddfde3e472e987b7290e05f3ad89ded97
Kristin Evans <kevans0625@gmail.com>
2020-05-23 16:37:32 -0500

    set up route elements

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,8 +10,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// // Add routes, both API and view
-// app.use("*");
+// Add routes, both API and view
+app.use("routes");
 
 
 
