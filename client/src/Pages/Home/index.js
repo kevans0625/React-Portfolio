2814509729c9eb5e12c3ad9131f59a64c07d069a
Kristin Evans <kevans0625@gmail.com>
2020-06-02 11:27:09 -0500

    updated homepage

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -4,15 +4,30 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <div className="cover-home container center center-align margin-bottom">
         <div className="cover">
             <div className="row">
                 <h2 className="center-align col-md-12">Hi, I am Kris, a full stack developer. I build and design responsive user interfaces.</h2>
-                <br></br>
-                <a href="/about" className=" btn">Enter Here <i className="material-icons">arrow_forward</i></a>
-            </div>
         </div>
+      {/* <div className="cover"> */}
+            <div className="row">
+            <span className="home-icons">
+            <i className="fab fa-css3-alt home-icons"></i>
+            <i className="fab fa-html5 home-icons"></i>
+            <i className="fab fa-js-square home-icons"></i>
+            <i className="fab fa-node home-icons"></i>
+            <i className="fab fa-bootstrap home-icons"></i>
+            <i className="fab fa-github-square home-icons"></i>
+            
+                 </span>
+                <br></br>
+
+      {/* </div> */}
+      <div className="center-align cover">
+                <a href="/about" className="btn center">Enter Here <i className="material-icons">arrow_forward</i></a>
+            </div>
+            </div>
       </div>
+
   </div>
 
 
